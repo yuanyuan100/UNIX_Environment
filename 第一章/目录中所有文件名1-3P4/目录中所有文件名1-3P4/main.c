3788a6c0b9ba30892eaa39527850d81995c0c372
yuanyuan100 <yuanyuan_pyy@163.com>
2017-06-28 14:58:53 +0800

    [add]增加MacOS的ignore

---
+++ 第一章/目录中所有文件名1-3P4/目录中所有文件名1-3P4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  目录中所有文件名1-3P4
+//
+//  Created by Mr.PYY on 17/6/28.
+//  Copyright © 2017年 Mr.PYY. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
